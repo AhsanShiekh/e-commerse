c3e99c0c2f2382eb6c7a422c1c264fe031209d39
AhsanShiekh <ahsanshiekh58@gmail.com>
2020-05-27 20:29:19 +0500

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import homepage from "./homepage.component";
 
 function App() {
   return (
     <div className="App">
-     hello
+     <homepage />
     </div>
   );
 }
