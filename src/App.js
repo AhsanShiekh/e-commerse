e2aa48902e79f9717c336103cf7c296c0920c456
AhsanShiekh <ahsanshiekh58@gmail.com>
2020-05-29 17:02:43 +0500

    Added a basic navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/Homepage.component';
 import ShopPage from './pages/shopPage/ShopPage.component';
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header.compnent";
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={Homepage}/>
-      <Route exact path='/shop' component={ShopPage}/>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
